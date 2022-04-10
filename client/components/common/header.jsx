3f090b7eb2962cdeb9096892559e2e0dcd198fca
tqb310 <baotq310@gmail.com>
2022-04-10 14:52:37 +0700

    Config: tailwindcss.config.js

--- client/components/common/header.jsx
+++ client/components/common/header.jsx
@@ -2,9 +2,8 @@
 
 export default function Header() {
     return (
-        <div className="flex">
-            <h1>Header</h1>
-            <h1>Header</h1>
+        <div className="flex bg-primary-color container">
+
         </div>
     );
 }
