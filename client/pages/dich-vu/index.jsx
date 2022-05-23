fb80bb93fd5fa2e6be76f1c658b2113dada4ceb7
liemdang260 <69330905+liemdang260@users.noreply.github.com>
2022-05-24 05:42:05 +0700

    done service introduction

--- client/pages/dich-vu/index.jsx
+++ client/pages/dich-vu/index.jsx
@@ -1,7 +1,7 @@
 import NewsWrapper from 'components/common/NewsWrapper';
 // import NewsCard from 'components/common/NewsCard';
 import { useRouter } from 'next/router';
-
+import Image from 'next/image';
 const data = [
     {
         id: 1,
@@ -66,58 +66,84 @@ const AllServices = () => {
             <p className='text-primary-color font-bold text-2xl text-center p-4'>
                 DỊCH VỤ
             </p>
-            {/* <div className='flex flex-wrap'>
-                {data &&
-                    data.map((item, index) => (
-                        <NewsCard key={index} item={item} />
-                    ))}
-            </div> */}
+
             <div className='border-8 rounded-[15px] border-double border-primary-color p-3'>
+                {/* <p>1. Tư Vấn, Thiết kế Nhà Gỗ: </p>
                 <p className='indent-[32px]'>
-                    Nhà gỗ 3 gian vốn không còn xa lạ với người dân Việt Nam,
-                    đặc biệt là vùng Bắc bộ, tuy nhiên, cùng với sự du nhập
-                    trong lối kiến trúc, nhà gỗ 3 gian đã có nhiều phát triển
-                    hơn trong nhận thức những người yêu kiến trúc. Nếu bạn muốn
-                    xây dựng một nhà gỗ 3 gian vừa mang nét đẹp truyền thống,
-                    vừa mang nét hiện đại của kiến trúc phương tây nhưng lo ngại
-                    về vấn đề giá cả, thì bài viết dưới đây, Nhà Gỗ Làng Mộc sẽ
-                    gửi đến bạn bảng báo giá chi tiết dịch vụ thiết kế - xây
-                    dựng nhà gỗ 3 gian do chúng tôi cung cấp.
+                    Chúng tôi cung cấp dịch vụ thiết kế tận tâm đến khách hàng,
+                    các bước trong dịch vụ thiết kế của chúng tôi gồm:
+                </p>
+                <p className='indent-[60px]'>
+                    - Gặp gỡ trao đổi với khách hàng
+                </p>
+                <div className='w-4/5 h-[330px] relative overflow-hidden'>
+                    <Image
+                        src='/gap-go-khach-hang.png'
+                        alt='gap-go-khach-hang'
+                        layout='fill'
+                        // className='hover:scale-105 ease-linear duration-150'
+                    ></Image>
+                </div>
+                <p className='indent-[60px]'>- Khảo sát địa hình thực tế</p>
+                <div className='w-4/5 h-[330px] relative overflow-hidden'>
+                    <Image
+                        src='/khao-sat-dia-hinh-thuc-te.png'
+                        alt='khao-sat-dia-hinh-thuc-te'
+                        layout='fill'
+                        // className='hover:scale-105 ease-linear duration-150'
+                    ></Image>
+                </div>
+                <p className='indent-[60px]'>
+                    - Lên ý tưởng (Diễn họa bằng mô hình, hình ảnh 3D)
+                </p>
+                <div className='w-4/5 h-[330px] relative overflow-hidden'>
+                    <Image
+                        src='/len-y-tuong.png'
+                        alt='len-y-tuong'
+                        layout='fill'
+                        // className='hover:scale-105 ease-linear duration-150'
+                    ></Image>
+                </div>
+                <div className='w-4/5 h-[330px] relative overflow-hidden'>
+                    <Image
+                        src='/y-tuong-3d.png'
+                        alt='y-tuong-3d'
+                        layout='fill'
+                        // className='hover:scale-105 ease-linear duration-150'
+                    ></Image>
+                </div>
+                <p className='indent-[60px]'>- Chốt ý tưởng với khách hang</p>
+                <div className='w-4/5 h-[330px] relative overflow-hidden'>
+                    <Image
+                        src='/chot-y-tuong.png'
+                        alt='chot-y-tuong'
+                        layout='fill'
+                        // className='hover:scale-105 ease-linear duration-150'
+                    ></Image>
+                </div> */}
+                <p className='indent-[32px]'>
+                    Với tay nghề truyền thống đã được khẳng định qua nhiều thế
+                    hệ, Nhà Gỗ Ba Miền luôn khẳng định được vị thế và thương
+                    hiệu hàng đầu Việt Nam về thiết kế, thi công nhà gỗ, nội
+                    thất và các sản phẩm từ gỗ.
                 </p>
                 <br />
-                <p className='indent-[32px] font-semibold'>
-                    Các yếu tố cấu thành giá làm nhà gỗ 3 gian:
-                </p>
                 <p className='indent-[32px]'>
-                    Để cấu thành nên tổng thể chi phí làm nhà gỗ 3 gian, ta cần
-                    tính toán chi tiết những yếu tố sau:
+                    Các dịch vụ chính của chúng tôi gồm:
                 </p>
-                <p className='indent-[32px] font-semibold'>Chi phí thiết kế:</p>
+                <ul className='list-disc ml-20'>
+                    <li>Tư vấn, thiết kế nhà gỗ</li>
+                    <li>Thi công nhà gỗ</li>
+                    <li>Tư vấn thiết kế và thi công nội thất</li>
+                </ul>
+                <br />
                 <p className='indent-[32px]'>
-                    Giai đoạn đầu tiên để hoàn thiện ngôi nhà gỗ trong mơ của
-                    bản thân chính là hoàn thiện chúng trên giấy, đây được gọi
-                    là giai đoạn thiết kế. Việc thiết kế giúp bạn và đội ngũ xây
-                    dựng xác định được hình dáng, kết cấu của ngôi nhà, từ đó dự
-                    trù được số lượng nguyên – vật liệu cần thiết, các bước tiến
-                    hành để hoàn thiện chúng. Ngoài ra bản vẽ cũng chính là cách
-                    để chúng ta kiểm soát được chất lượng của thành phẩm. Thông
-                    thường một bộ bản vẽ thiết kế căn nhà đầy đủ sẽ gồm 4 loại
-                    bản vẽ:
-                </p>
-                <p className='indent-[60px]'>- Bản vẽ kiến trúc</p>
-                <p className='indent-[60px]'>- Bản vẽ kết cấu</p>
-                <p className='indent-[60px]'>- Bản vẽ điện, nước</p>
-                <p className='indent-[60px]'>- Bản vẽ nội, ngoại thất</p>
-                <p className='indent-[32px]'>
-                    Hiện nay, mức giá thiết kế phổ biến cho một căn nhà gỗ có
-                    diện tích dưới 120m2 là 2 triệu đồng, trên 120m2 sẽ có giá
-                    65.000đ/m2. Ngoài ra, nếu bạn có kỹ năng chuyên môn về thiết
-                    kế, có sẵn ý tưởng trong đầu, bạn hoàn toàn có thể tiết kiệm
-                    chi phí bằng cách tự tạo lập hay tính toán các bản vẽ trên.
-                    Nếu không có thời gian hoặc không chắc chắn về khả năng
-                    thiết kế, bạn có thể tìm đến sự giúp đỡ từ những công ty,
-                    doanh nghiệp hoặc kiến trúc sư chuyên nghiệp với một khoảng
-                    phí như trên.
+                    Nhà gỗ Ba Miền đã thiết kế và thi công rất nhiều công trình
+                    trên khắp mọi miền đất nước. Đến với Nhà gỗ Ba Miền quý
+                    khách được tư vấn tỉ mỷ đến từng chi tiết, mọi thắc mắc của
+                    Quý khách hàng chúng tôi luôn sãn sàng giải đáp.... Đến với
+                    nhà Gỗ Ba Miền Quý khách sẽ luôn hài lòng với đội ngũ nhân
+                    viên nhiều kinh nghiệm, nhiệt tình, tận tâm.
                 </p>
             </div>
         </NewsWrapper>
@@ -125,4 +151,3 @@ const AllServices = () => {
 };
 
 export default AllServices;
-
