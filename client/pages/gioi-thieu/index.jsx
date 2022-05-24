0052bf5a7199f3d7dc8c090d42df278131ee6a63
liemdang260 <69330905+liemdang260@users.noreply.github.com>
2022-05-24 18:16:18 +0700

    done subservices

--- client/pages/gioi-thieu/index.jsx
+++ client/pages/gioi-thieu/index.jsx
@@ -1,87 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import NewsWrapper from 'components/common/NewsWrapper';
-const data = [
-    {
-        step: 1,
-        title: 'Khảo sát mặt bằng, lấy ý tưởng khách hàng',
-        imgLink: '/khao-sat-mat-bang.png',
-        alt: 'khao-sat-mat-bang.png',
-    },
-    {
-        step: 2,
-        title: 'Tư vấn, thiết kế',
-        imgLink: '/tu-van-thiet-ke.png',
-        alt: 'tu-van-thiet-ke.png',
-    },
-    {
-        step: 3,
-        title: 'Chọn gỗ ( chủng loại )',
-        imgLink: '/chongo.png',
-        alt: 'chongo',
-    },
-    {
-        step: 4,
-        title: 'Cắt đầu gỗ ( lễ phạt mộc)',
-        imgLink: '/phatmoc.png',
-        alt: 'phatmoc',
-    },
-    {
-        step: 5,
-        title: 'Sàm Đóng',
-        imgLink: '/samdong.png',
-        alt: 'samdong',
-    },
-    {
-        step: 6,
-        title: 'Đục Trạm',
-        imgLink: '/duccham.png',
-        alt: 'duccham',
-    },
-    {
-        step: 7,
-        title: 'Làm lót',
-        imgLink: '/lamlot.png',
-        alt: 'lamlot',
-    },
-    {
-        step: 8,
-        title: 'Vận chuyển đến công trình',
-        imgLink: '/vanchuyen.png',
-        alt: 'vanchuyen',
-    },
-    {
-        step: 9,
-        title: 'Lắp dựng',
-        imgLink: '/lapdung.png',
-        alt: 'lapdung',
-    },
-    {
-        step: 10,
-        title: 'Sơn hoàn thiện',
-        imgLink: '/sonhoanthien.png',
-        alt: 'sonhoanthien',
-    },
-];
-const Step = ({ step, title, imgLink, alt }) => {
-    return (
-        <div className='inline-block w-[230px] h-[275px] mt-5 border-2 rounded-md overflow-hidden'>
-            <div className='w-full h-[220px] relative overflow-hidden'>
-                <Image
-                    src={imgLink}
-                    alt={alt}
-                    layout='fill'
-                    className='hover:scale-105 ease-linear duration-150'
-                ></Image>
-            </div>
-            <div className='bg-primary-color p-1 h-[55px] flex justify-center flex-col'>
-                <p className='text-white align-middle text-center text-base'>
-                    <span className='underline'>Bước {step}</span>: {title}
-                </p>
-            </div>
-        </div>
-    );
-};
+import ConstructionSteps from 'components/common/ConstructionSteps';
+
 const Introduction = () => {
     return (
         <NewsWrapper pathname={{ title: 'Giới Thiệu', link: '/gioi-thieu' }}>
@@ -91,19 +12,7 @@ const Introduction = () => {
             <br />
             <div className='border-2 p-3 text-sm'>
                 <p className='text-base'>Các Công đoạn triển khai Công trình</p>
-                <div className='flex justify-around flex-wrap'>
-                    {data.map((d, index) => {
-                        return (
-                            <Step
-                                key={index}
-                                step={d.step}
-                                title={d.title}
-                                imgLink={d.imgLink}
-                                alt={d.alt}
-                            ></Step>
-                        );
-                    })}
-                </div>
+                <ConstructionSteps />
             </div>
         </NewsWrapper>
     );
