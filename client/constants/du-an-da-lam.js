4c2a0fdaca519caec26a29eefd8b2c83ca81debc
tqb310 <baotq310@gmail.com>
2022-06-20 23:31:43 +0700

    Add media

--- client/constants/du-an-da-lam.js
+++ client/constants/du-an-da-lam.js
@@ -9,4 +9,30 @@ const projects = {
     'do-tho': 'Đồ thờ',
 };
 
+export const projectMedia = [
+    {
+        id: 1,
+        title: 'Nội thất',
+        slug: 'noi-that',
+        media: [
+            {
+                type: 'image',
+                link: 'https://lh3.google.com/u/0/d/1jNQAvCEMkJA_OSjH4PsYFoou8QnYmWSb=w1920-h950-iv1',
+            },
+            {
+                type: 'video',
+                link: 'link video',
+            },
+            //more...
+        ],
+    },
+    {
+        id: 2,
+        title: 'Nhà lục giác',
+        slug: 'nha-luc-giac',
+        media: [
+            //...
+        ],
+    },
+];
 export default projects;
