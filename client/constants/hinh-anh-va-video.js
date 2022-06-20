d6883734ea9718f08dc2f7d5dd9994f60af26efc
Tien Nguyen <18521487@gm.it.edu.vn>
2022-06-20 23:13:51 +0700

    add file data hinh-anh-va-video

---
+++ client/constants/hinh-anh-va-video.js
@@ -0,0 +1,27 @@
+export default {
+    avatarDichVu: {
+        linkImage: [
+            {
+                name: 'tuvan&thietke',
+                link: 'https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc',
+            },
+            // more....
+        ],
+        linkVideo: [
+            {
+                name: 'tuvan&thietke',
+                link: 'https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc',
+            },
+            //more
+        ],
+    },
+
+    duAnDaLam: [
+        {
+            avatarDalam: [
+                // Same above
+            ],
+        },
+        // more...
+    ],
+};
